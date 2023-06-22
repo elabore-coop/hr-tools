0e80defb81e1c025e27ffa4db00e3e88c3505b3d
clementthomas <clement@cotyl.net>
2023-06-22 14:28:18 +0200

    [NEW] hr_expense_analytic_account_preselect_with_project: new module

---
+++ hr_expense_analytic_account_preselect_with_project/models/hr_expense.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models, api
+
+class HrExpense(models.Model):
+    _inherit = "hr.expense"
+
+    project_id = fields.Many2one('project.project')
+
+    @api.onchange('project_id')
+    def set_analytic_account(self):
+        self.analytic_account_id = self.project_id.analytic_account_id
+        
