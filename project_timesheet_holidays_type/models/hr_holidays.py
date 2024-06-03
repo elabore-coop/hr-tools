f5377a3af3ec35c9142fecf541803bedb2daac70
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2024-06-03 15:11:50 +0200

    [ADD]project_timesheet_holidays_type

--- project_timesheet_holidays_type/models/hr_holidays.py
+++ project_timesheet_holidays_type/models/hr_holidays.py
@@ -8,5 +8,5 @@ class Holidays(models.Model):
 
     def _timesheet_prepare_line_values(self, index, work_hours_data, day_date, work_hours_count):
         res = super()._timesheet_prepare_line_values(index, work_hours_data, day_date, work_hours_count)
-        res['name'] = _("%s (%s/%s)",self.holiday_status_id.name, index + 1, len(work_hours_data))
+        res['name'] = _("%s",self.holiday_status_id.name)
         return res
