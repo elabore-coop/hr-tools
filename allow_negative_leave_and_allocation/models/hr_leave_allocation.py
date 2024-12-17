161a48f222371c5c0987b501535bef69d3861d1e
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2024-12-17 12:48:07 +0100

    [IMP]allow_negative_leave_and_allocation:add negative allocation

---
+++ allow_negative_leave_and_allocation/models/hr_leave_allocation.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (c) 2005-2006 Axelor SARL. (http://www.axelor.com)
+
+from odoo import models
+
+class HrLeaveAllocation(models.Model):
+    _inherit = "hr.leave.allocation"
+
+    _sql_constraints = [
+        ('type_value',
+         "CHECK( (holiday_type='employee' AND (employee_id IS NOT NULL OR multi_employee IS TRUE)) or "
+         "(holiday_type='category' AND category_id IS NOT NULL) or "
+         "(holiday_type='department' AND department_id IS NOT NULL) or "
+         "(holiday_type='company' AND mode_company_id IS NOT NULL))",
+         "The employee, department, company or employee category of this request is missing. Please make sure that your user login is linked to an employee."),
+        ('duration_check', "CHECK((allocation_type != 'regular'))", "The duration must be greater than 0."),
+    ]
